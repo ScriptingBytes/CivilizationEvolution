ef181f589db1f4a9f824d239cd88124c4cab0f12
YouRock1010 <thomaskkyrouac@gmail.com>
2023-05-29 15:53:02 -0500

    v0.0.1

---
+++ game/main.js
@@ -0,0 +1,16 @@
+var GameStorage = {
+    Evolution: {},
+    Resources: {},
+    Research: {},
+    City: {},
+    Stats: {start:Date.now(),days:0,tdays:0}
+}
+
+var Tabs = {
+    Evolve: {},
+    City: {},
+    Research: {},
+    Resources: {},
+    Government: {},
+    Settings: {},
+}
